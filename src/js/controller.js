e34856031ede622fc3747e6460eee3c46f992a19
Bhuvan-NM <bhuvanarasimha29@gmail.com>
2024-02-07 17:03:26 +1100

    added added some functionallity, and parcel

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,4 +1,10 @@
-const recipeContainer = document.querySelector('.recipe');
+import * as model from "./model.js";
+import recipeView from "./views/recipeView.js";
+
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
+const recipeContainer = document.querySelector(".recipe");
 
 const timeout = function (s) {
   return new Promise(function (_, reject) {
@@ -11,3 +17,28 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+
+    recipeView.renderSpinner();
+
+    //Loding Recipe
+    await model.loadRecipe(id);
+
+    //Rendering the Recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    console.log(err);
+  }
+};
+controlRecipes();
+
+["hashchange", "load"].forEach((ev) =>
+  window.addEventListener(ev, controlRecipes)
+);
+
+window.addEventListener("hashchange", controlRecipes);
